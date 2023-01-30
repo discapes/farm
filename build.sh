319fb54e4979cc6dd337905fb86efd5d3c2067a3
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-01-30 02:32:17 +0200

    change actions to maven

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# If you have VS Code, just use the default build task
-
-shopt -s globstar
-javac -d ./bin ./src/main/java/**/*.java
-cd bin
-jar cmvf ../META-INF/MANIFEST.MF ../farm.jar **/*.class
