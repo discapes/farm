e40fd1346a92daa93346a6148aac25ab08df92b1
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-01-31 19:59:05 +0200

    clean things

--- build-serialization.sh
+++ build-serialization.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is to enable GraalVM native image building, because serialization requires a special config file.
 # Make sure you have GraalVM installed and run `gu install native-image`
-java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar farm-1.0-SNAPSHOT.jar
+java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/farm-1.0-SNAPSHOT.jar
 # Launch "x64 Native Tools Command Prompt for VS 2022"
 # cd target 
 # native-image -jar farm-1.0-SNAPSHOT.jar
